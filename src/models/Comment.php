4cb77ad763b0f91300c2f59ac258ba1a9ba156eb
Warez-amasya <thimote.cabotte6259@gmail.com>
2024-06-11 14:23:00 +0200

    fix comment

--- src/models/Comment.php
+++ src/models/Comment.php
@@ -27,78 +27,141 @@ class Comment
     // Titre du post auquel le commentaire est associé
     private string $postTitle;
 
-    // Getters et setters pour l'identifiant du commentaire
+    /**
+     * Obtient l'identifiant du commentaire.
+     *
+     * @return int
+     */
     public function getId(): int
     {
         return $this->id;
     }
 
+    /**
+     * Définit l'identifiant du commentaire.
+     *
+     * @param int $id Identifiant du commentaire
+     */
     public function setId(int $id): void
     {
         $this->id = $id;
     }
 
-    // Getters et setters pour le contenu du commentaire
+    /**
+     * Obtient le contenu du commentaire.
+     *
+     * @return string
+     */
     public function getContent(): string
     {
         return $this->content;
     }
 
+    /**
+     * Définit le contenu du commentaire.
+     *
+     * @param string $content Contenu du commentaire
+     */
     public function setContent(string $content): void
     {
         $this->content = $content;
     }
 
-    // Getters et setters pour l'identifiant du post associé au commentaire
+    /**
+     * Obtient l'identifiant du post associé au commentaire.
+     *
+     * @return int
+     */
     public function getPostId(): int
     {
         return $this->postId;
     }
 
+    /**
+     * Définit l'identifiant du post associé au commentaire.
+     *
+     * @param int $postId Identifiant du post
+     */
     public function setPostId(int $postId): void
     {
         $this->postId = $postId;
     }
 
-    // Getters et setters pour l'identifiant de l'utilisateur qui a créé le commentaire
+    /**
+     * Obtient l'identifiant de l'utilisateur qui a créé le commentaire.
+     *
+     * @return int
+     */
     public function getUserId(): int
     {
         return $this->userId;
     }
 
+    /**
+     * Définit l'identifiant de l'utilisateur qui a créé le commentaire.
+     *
+     * @param int $userId Identifiant de l'utilisateur
+     */
     public function setUserId(int $userId): void
     {
         $this->userId = $userId;
     }
 
-    // Getters et setters pour le nom d'utilisateur de l'auteur du commentaire
+    /**
+     * Obtient le nom d'utilisateur de l'auteur du commentaire.
+     *
+     * @return string
+     */
     public function getUsername(): string
     {
         return $this->username;
     }
 
+    /**
+     * Définit le nom d'utilisateur de l'auteur du commentaire.
+     *
+     * @param string $username Nom d'utilisateur
+     */
     public function setUsername(string $username): void
     {
         $this->username = $username;
     }
 
-    // Getters et setters pour indiquer si le commentaire est confirmé ou non
+    /**
+     * Indique si le commentaire est confirmé ou non.
+     *
+     * @return bool
+     */
     public function isConfirmed(): bool
     {
         return $this->isConfirmed;
     }
 
+    /**
+     * Définit si le commentaire est confirmé ou non.
+     *
+     * @param bool $isConfirmed Indique si le commentaire est confirmé
+     */
     public function setIsConfirmed(bool $isConfirmed): void
     {
         $this->isConfirmed = $isConfirmed;
     }
 
-    // Getters et setters pour le titre du post associé au commentaire
+    /**
+     * Obtient le titre du post associé au commentaire.
+     *
+     * @return string
+     */
     public function getPostTitle(): string
     {
         return $this->postTitle;
     }
 
+    /**
+     * Définit le titre du post associé au commentaire.
+     *
+     * @param string $postTitle Titre du post
+     */
     public function setPostTitle(string $postTitle): void
     {
         $this->postTitle = $postTitle;
