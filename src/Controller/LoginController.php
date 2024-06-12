845061da11dd84a14d089dd2a5147a8a5f643d33
Warez-amasya <thimote.cabotte6259@gmail.com>
2024-06-12 11:33:22 +0200

    fix code

--- src/Controller/LoginController.php
+++ src/Controller/LoginController.php
@@ -56,7 +56,7 @@ class LoginController extends BaseController
     private function getRequestMethod(): string
     {
         $requestMethod = $_SERVER['REQUEST_METHOD'] ?? '';
-        $sanitizedRequestMethod = filter_var($requestMethod, FILTER_SANITIZE_STRING, FILTER_FLAG_NO_ENCODE_QUOTES);
+        $sanitizedRequestMethod = filter_var(stripslashes($requestMethod), FILTER_SANITIZE_FULL_SPECIAL_CHARS);
         return $sanitizedRequestMethod !== false ? $sanitizedRequestMethod : '';
     }
 
